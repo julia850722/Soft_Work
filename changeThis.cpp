56513d815e2bcc76eaa8308c01d587b89a5b13f7
Pei726 <pirate100823@gmail.com>
2017-10-25 17:58:28 +0800

    Second 00357133

--- changeThis.cpp
+++ changeThis.cpp
@@ -1,1 +1,12 @@
+include<stdio.h>
+int main()
+{
+	printf("中文：你好可憐\n") ;
+	printf("日文：あなたはとても貧しい\n") ;
+	printf("俄文：Ты такой бедный\n") ;
+	printf("丹麥文：Du er så dårlig\n") ;
+	printf("希臘文：Είσαι τόσο φτωχός\n") ;
+	printf("蒙古文：تون ڏاڍو غريب آهين\n") ;
+	return 0 ;
+}
 
